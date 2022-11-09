e159c996237c937d42b4269f9e79d2425085bb4e
Felipe B. de Moura <fittato@gmail.com>
2022-11-08 22:07:56 -0300

    Finalizada aula 3

--- bytebank/src/CriaConta.java
+++ bytebank/src/CriaConta.java
@@ -9,6 +9,22 @@ public class CriaConta {
 		
 		System.out.println(primeiraConta.saldo);
 		
+		primeiraConta.saldo += 100;
+		
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		
+		segundaConta.saldo = 50;
+		
+		System.out.println("Primeira conta tem " + primeiraConta.saldo);
+		
+		System.out.println("Segunda conta tem " + segundaConta.saldo);
+		
+		System.out.println(primeiraConta.agencia);
+		
+		System.out.println(primeiraConta.numero);
+		
 	}
 
 }
