c723698d83386b944724b299490ed52ef1549177
Felipe B. de Moura <fittato@gmail.com>
2023-01-05 16:23:40 -0300

    Finalizada a aula 02 do curso Java e java.util

--- bytebank_herdado_conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ bytebank_herdado_conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,30 +1,40 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Object[] referencias = new Object[5];
 		
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
 		
-		contas[0] = cc1;
+		referencias[0] = cc1;
 		
-		ContaCorrente cc2 = new ContaCorrente(22, 22);
+		ContaPoupanca cc2 = new ContaPoupanca(22, 22);
 		
-		contas[1] = cc2;
+		referencias[1] = cc2;
+		
+		Cliente cliente = new Cliente();
+		
+		referencias[2] = cliente;
 		
 		// System.out.println(cc2.getNumero());
 		
-		System.out.println(contas[1].getNumero());
+//		Object ReferenciaGenerica = contas[1];
+//		
+//		System.out.println(ReferenciaGenerica.getNumero());
 		
-		 ContaCorrente ref = contas[1];
+		// Isso é um type cast. Transforma o tipo mais genérico para o tipo mais específico
+		ContaPoupanca ref = (ContaPoupanca) referencias[1];
 		 
-		 System.out.println(cc2.getNumero());
+		System.out.println(cc2.getNumero());
 		 
-		 System.out.println(ref.getNumero());
+		System.out.println(ref.getNumero());
 	}
 
 }
