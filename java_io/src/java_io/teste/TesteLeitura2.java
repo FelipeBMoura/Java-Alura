981fe6d6d40cd402c39cd5dcf118ae83f54c53d1
Felipe B. de Moura <fittato@gmail.com>
2023-01-31 18:47:32 -0300

    Finalizada a aula 04 do curso Java e java.io

---
+++ java_io/src/java_io/teste/TesteLeitura2.java
@@ -0,0 +1,39 @@
+package java_io.teste;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+
+	public static void main(String[] args) throws Exception {
+		
+		Scanner scanner = new Scanner(new File("contas.csv"));
+		
+		while(scanner.hasNextLine()) {
+			String linha = scanner.nextLine();
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US); // Define o padrão de regra de escrita por local
+			linhaScanner.useDelimiter(",");
+			
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
+			
+			// %s significa formatação de string, %f significa formatação de double
+			String valorFormatado = String.format(new Locale("pt", "BR"), "%s - %04d-%08d, %20s: %08.2f %n", tipoConta, agencia, numero, titular, saldo);
+			System.out.println(valorFormatado);
+			
+//			String[] valores = linha.split(",");
+//			System.out.println(Arrays.toString(valores));
+		}
+		
+		scanner.close();
+
+	}
+
+}
