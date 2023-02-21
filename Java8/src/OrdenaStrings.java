3d85aef26e25735bf52731edf904884e1e23453b
Felipe B. de Moura <fittato@gmail.com>
2023-02-21 17:12:09 -0300

    Finalizada a aula 03do curso Java 8 conhecendo as novidades dessa versão

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
 
@@ -11,10 +11,19 @@ public class OrdenaStrings {
 		palavras.add("editora casa do código");
 		palavras.add("caelum");
 		
-		Comparator<String> comparador = new ComparadorPorTamanho();	
+//		Comparator<String> comparador = new ComparadorPorTamanho();	
 		
 //		Collections.sort(palavras, comparador);
 		
+//		palavras.sort(comparador);
+		
+//		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+//		palavras.sort(Comparator.comparing(s -> s.length()));
+		
+		// Para ficar mais legível o lambda acima, a mesma função foi quebrada nas 3 linhas abaixo
+		Function <String, Integer> funcao = s -> s.length(); // ou String::length ao invés de s -> s.length(
+		Comparator<String> comparador = Comparator.comparing(funcao);
 		palavras.sort(comparador);
 		
 		System.out.println(palavras);
@@ -25,19 +34,42 @@ public class OrdenaStrings {
 
 //		Consumer<String> consumidor = null;
 		
-		Consumer<String> consumidor = new ImprimeNaLinha();		
-		palavras.forEach(consumidor);
+//		Consumer<String> consumidor = new ImprimeNaLinha();
+		
+//		palavras.forEach(consumidor);
+		
+//		Consumer<String> consumidor = new Consumer<String>() {
+//			
+//			@Override
+//			public void accept(String s) {
+//				System.out.println(s);
+//			}
+//		};
+		
+//		palavras.forEach(new Consumer<String>() {
+//			
+//			@Override
+//			public void accept(String s) {
+//				System.out.println(s);
+//			}
+//		});
+		
+//		palavras.forEach((String s) -> {
+//				System.out.println(s);
+//		});
+		
+		palavras.forEach(s -> System.out.println(s));
 	}
 
 }
 
-class ImprimeNaLinha implements Consumer<String>{
-	
-	@Override
-	public void accept(String s) {
-		System.out.println(s);;
-	}
-}
+//class ImprimeNaLinha implements Consumer<String>{
+//	
+//	@Override
+//	public void accept(String s) {
+//		System.out.println(s);;
+//	}
+//}
 
 class ComparadorPorTamanho implements Comparator<String>{
 	
